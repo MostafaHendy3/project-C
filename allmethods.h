20304323fe1df4e914bd33656449a021ae60df30
Aya <ayafarid700@gmail.com>
2023-02-15 16:40:38 +0200

    New code

--- allmethods.h
+++ allmethods.h
@@ -6,11 +6,11 @@
 #define UNTITLED11_ALLMETHODS_H
 #include "struct.h"
 #include "data.h"
-#define Adminpassword 1234
+char* Adminpassword="1234";
 //1 aya
 void addStudentRecord(Student *st);
-void editStudentGrade(int pass);
-void editAdminPassword(int pass);
+void editStudentGrade();
+void editAdminPassword();
 
 //2 salma
 void removeStudentRecord(int id);
@@ -20,5 +20,6 @@ void editStudentName();
 //3 me
 void viewStudentRecord(int id);
 void viewAllStudentRecord();
+Student * readStudent();
 
 #endif //UNTITLED11_ALLMETHODS_H
